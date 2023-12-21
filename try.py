ea4dd8bcf49f66587057ce6c6f10b314d9c16c57
sundim <sunxim@yandex.com>
2023-12-20 17:39:41 -0800

    Delete unnecessary files and code

--- try.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-ls = list(map(int, input().split(" ")))
-steps = 0
-mxi = ls.index(max(ls))
-mni = n-(n-ls[::-1].index(min(ls)))-1
-steps += mxi+mni
-if ls.index(min(ls))>mxi:
-  steps+=1
-print(steps)
